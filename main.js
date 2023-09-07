9812ab5ea2bd8b4257595f657389856ecabcb5b9
sem-aa <66129330+sem-aa@users.noreply.github.com>
2023-09-07 12:39:09 +0300

    add zero

---
+++ main.js
@@ -0,0 +1,42 @@
+const circle = document.querySelector(".circle");
+const btnStart = document.querySelector(".btn-start");
+const point = document.querySelector(".point");
+const inputNumber = document.querySelector(".input-number");
+const playNumber = document.querySelector(".play-number");
+const playColor = document.querySelector(".play-color");
+const winNumberSpan = document.querySelector(".win-number");
+const winColor = document.querySelector(".win-color");
+
+btnStart.onclick = () => {
+  if (inputNumber.value < 0 || inputNumber.value > 36) {
+    alert("Введите номер от 0 до 36");
+    inputNumber.value = "";
+    return;
+  }
+  btnStart.disabled = true;
+
+  playNumber.innerHTML = inputNumber.value;
+  if (Number(inputNumber.value) === 0) {
+    playColor.innerHTML = "green";
+  } else {
+    playColor.innerHTML = inputNumber.value % 2 === 0 ? "red" : "black";
+  }
+
+  circle.style.transform = "rotate(0deg)";
+  let number = Math.ceil(Math.random() * 1000);
+  circle.style.transform = `rotate(${number}deg)`;
+  const x = point.getBoundingClientRect().x;
+  const y = point.getBoundingClientRect().y;
+
+  setTimeout(() => {
+    const winNumber = document.elementFromPoint(x, y + 5).textContent;
+    if (Number(winNumber) === 0) {
+      winColor.innerHTML = "green";
+    } else {
+      winColor.innerHTML = winNumber % 2 === 0 ? "red" : "black";
+    }
+    winNumberSpan.innerHTML = winNumber;
+    inputNumber.value = "";
+    btnStart.disabled = false;
+  }, 5500);
+};
